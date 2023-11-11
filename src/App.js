dae1e97e41f7fc0b24bfd611a79a8fd97445ddb4
chado <a.sale@outlook.fr>
2023-11-11 08:02:36 +0100

    Feat: Add routes in porject

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import React from "react";
-import Home from "./component/home";
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './component/home';
+import {Contact} from './component/Contact';
+import {Skill} from './component/Skill';
 
 function App() {
-  return (
-    <div className="App">
-      <Home className="home"></Home>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/skill" element={<Skill/>}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
