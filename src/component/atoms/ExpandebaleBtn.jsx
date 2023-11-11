1fe88a6eca9aea3340381396d42555d424b12b15
chado <a.sale@outlook.fr>
2023-11-11 08:04:39 +0100

    Feat: refont expandebaleBtn

--- src/component/atoms/ExpandebaleBtn.jsx
+++ src/component/atoms/ExpandebaleBtn.jsx
@@ -1,83 +1,63 @@
-import React from "react";
+import React, {useState } from "react";
 import vector1 from "../../images/vector/Vector1.svg";
 import vector2 from "../../images/vector/Vector2.svg";
-import {useEffect, useState} from "react";
 
 export function ExpandebaleBtn() {
 
-    const [spitchWidth, setSpitchWidth] = useState(0);
-        useEffect(() => {
-            const spitch = document.querySelector('.spitch');
-            const handleTransitionEnd = () => {
-                setTimeout(() => {
-                    const width = spitch.getBoundingClientRect().width;
-                    if (width > 50) { // Remplacez 50 par la largeur à laquelle vous voulez que le code soit exécuté
-                       spitch.classList.remove('hidden')
-                    }
-                }, 1000); // Remplacez 1000 par le temps en millisecondes après lequel vous voulez que le code soit exécuté
-            };
-            spitch.addEventListener('transitionend', handleTransitionEnd);
-            return () => {
-                spitch.removeEventListener('transitionend', handleTransitionEnd);
-            };
-        }, []);
-
-        let ClassTitle = 'text-5xl font-bold text-center font-[DeathStar] text-shadow';
-    let StyleTitle = {
-        color: 'transparent',
-        WebkitTextStroke: '1px #CEB7FF',
-    }
-
-    let StyleSpitch = {
-        transition: "width 1s, height 1s",
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+        setIsOpen(!isOpen);
     };
-    function openSpitch() {
-        let spitch = document.querySelector('.spitch');
-        let button = document.querySelector('.button-open');
 
-        if (spitch.classList.contains('w-0')) {
-            spitch.classList.remove('w-0');
-            spitch.classList.remove('h-0');
-            spitch.classList.add('w-96');
-            spitch.classList.add('h-24');
-            button.classList.add('hidden');
-        } else {
-            spitch.classList.remove('w-96');
-            spitch.classList.remove('h-24');
-            spitch.classList.add('w-0');
-            spitch.classList.add('h-0');
-        }
+    const ClassTitle = "text-5xl font-bold text-center font-[DeathStar] text-shadow";
+    const StyleTitle = {
+        color: "transparent",
+        WebkitTextStroke: "1px #CEB7FF",
+        transition: isOpen ? "width 1s, height 1s, opacity 1s" : "width 1s , height 1s, transform 1s",
+        transform: isOpen ? "scale(0)" : "scale(1)",
+        hidden: isOpen ? "opacity 0" : "opacity 1",
+        opacity: isOpen ? 0 : 1,
+        width: isOpen ? "0" : "auto",
+        height: isOpen ? "0" : "auto",
+    };
 
-        spitch.addEventListener('transitionend', () => {
-            let spitchWidth = spitch.offsetWidth;
-            if (spitchWidth === 0) {
-                button.classList.remove('hidden');
-            }
-        });
-    }
-
+    const StyleSpitch = {
+        // display: isOpen ? "inline" : "none",
+        height: isOpen ? "200px" : "0",
+        width: isOpen ? "750px" : "0",
+        fontFamily: "DeathStar",
+        fontSize: "25px",
+        color: "#CEB7FF",
+        border: "3px solid #CEB7FF",
+        background: 'rgba(47, 47, 56, 0.7)',
+        transition: isOpen ? "height 1s, width 1s, display 1s" : "height 1s , width 1s, display 1s",
+        transform: isOpen ? "scale(1)" : "scale(0)",
+    };
+    let containerExpandebale = {
+        transition: isOpen ? "width 1s, height 1s" : "width 1s , height 1s",
+        // width: isOpen ? "750px" : "auto",
+    };
     return (
-        <div className={'w-fit h-fit grid grid-cols-11 cursor-pointer'} onClick={openSpitch}>
-            <div className={'grid col-start-1 col-end-12'}>
-                <img src={vector1} alt="vector1" className={''}/>
+        <div
+            className={"w-fit h-fit p-8 relative cursor-pointer justify-center items-center"}
+            style={containerExpandebale}
+            onClick={handleClick}
+        >
+            <div className={"absolute top-[0] left-[0] col-start-1 col-end-12"}>
+                <img src={vector1} alt="vector1" className={""} />
             </div>
-            <div className={'grid col-start-2 col-end-10 justify-center'}>
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a className={'w-fit'} >
-                    <h2 className={ClassTitle + ' button-open'} style={StyleTitle}>
-                        CLICK <br/> TO OPEN
-                    </h2>
-                </a>
-
-                {/* eslint-disable-next-line react/style-prop-object */}
-                <div className={'w-0 h-0 overflow-hidden spitch'} style={StyleSpitch}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, atque, commodi dicta doloremque eos facilis hic incidunt ipsa nesciunt nihil nobis porro quis, rerum sequi voluptate. Debitis minus nobis repellendus.
-                </div>
-
+            <div className={"flex justify-center items-center"} >
+                <h2 className={ClassTitle} style={StyleTitle}>
+                    CLICK <br /> TO OPEN
+                </h2>
+                <span className={"overflow-hidden text-center p-6 spitch"} style={StyleSpitch}>
+                    Mon objectif est de transformer vos idees en realite numerique. Je suis axe sur la qualite du code,
+                    la securite et l'efficacite, et je suis capable de gerer tous les aspects de la conception de votre code.
+                </span>
             </div>
-            <div className={'grid col-start-1 col-end-12 justify-end'}>
-                <img src={vector2} alt="vector2" className={''}/>
+            <div className={"absolute bottom-[0] right-[0] col-start-1 col-end-12 justify-end"}>
+                <img src={vector2} alt="vector2" className={""} />
             </div>
         </div>
-    )
+    );
 }
