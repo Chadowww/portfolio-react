529dcefaa56501b7aeea5b4b28255427d8472133
chado <a.sale@outlook.fr>
2023-11-11 08:06:13 +0100

    Feat: update component to previews

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import Home from "../component/home";
+import {ExpandebaleBtn} from "../component/atoms/ExpandebaleBtn";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/Home">
                 <Home/>
+            </ComponentPreview>
+            <ComponentPreview path="/ExpandebaleBtn">
+                <ExpandebaleBtn/>
             </ComponentPreview>
         </Previews>
     )
