59754e34adb6842671c8d9716910d6ecb8eb01ad
chado <a.sale@outlook.fr>
2023-11-12 07:54:15 +0100

    Feat: add loading page WIP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './component/home';
-import {Contact} from './component/Contact';
-import {Skill} from './component/Skill';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './component/pages/home';
+import { Contact } from './component/pages/Contact';
+import { Skill } from './component/pages/Skill';
+import {AnimatePresence} from "framer-motion";
 
 function App() {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/skill" element={<Skill/>}></Route>
-            </Routes>
+            <AnimatePresence>
+                {loading ? (
+                    <div className="loader-container">
+                        <div className="spinner"></div>
+                    </div>
+                ) : (
+                <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/skill" element={<Skill />} />
+                </Routes>
+                )}
+            </AnimatePresence>
         </Router>
     );
 }
