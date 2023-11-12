86a56b8c00c30f47fa792fd4e0b6cf88a5499162
chado <a.sale@outlook.fr>
2023-11-12 08:00:25 +0100

    Feat: add transition for loading page WIP

--- src/component/pages/home.jsx
+++ src/component/pages/home.jsx
@@ -1,8 +1,9 @@
 import * as PropTypes from "prop-types";
-import background from '../images/bg-home.png';
-import {MainHome} from "./molecules/MainHome";
-import {RightSideBar} from "./molecules/RightSideBar";
-import {LeftSideBar} from "./molecules/LeftSideBar";
+import {MainHome} from "../organisms/MainHome";
+import {RightSideBar} from "../organisms/RightSideBar";
+import {LeftSideBar} from "../organisms/LeftSideBar";
+import background from '../../images/bg-home.png';
+import {motion} from "framer-motion";
 
 function Home(props) {
     const ClassHome = ' ' + props.className || '';
@@ -12,15 +13,32 @@ function Home(props) {
         backgroundRepeat: 'no-repeat',
     }
 
+    const pageVariants = {
+        initial: { opacity: 0},
+        animate: { opacity: 1 },
+        exit: { opacity: 0 },
+    };
+
+    const pageTransition = {
+        duration: 4,
+    };
 
     return(
-        <div className={ClassHome} style={StyleHome}>
-            <div className={"h-screen w-full flex "} style={{background: 'rgba(47, 47, 56, 0.4)',}}>
-                <LeftSideBar/>
-                <MainHome/>
-                <RightSideBar/>
+        <motion.div
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageVariants}
+            transition={pageTransition}
+        >
+            <div className={ClassHome} style={StyleHome}>
+                <div className={"h-screen w-full flex "} style={{background: 'rgba(47, 47, 56, 0.4)',}}>
+                    <LeftSideBar/>
+                    <MainHome/>
+                    <RightSideBar/>
+                </div>
             </div>
-        </div>
+        </motion.div>
     );
 }
 
