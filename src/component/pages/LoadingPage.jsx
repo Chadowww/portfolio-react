7b9c66b967bf20789459f448c9486802be8c305d
chado <a.sale@outlook.fr>
2023-11-13 07:03:05 +0100

    Feat: Create Loadiing page

---
+++ src/component/pages/LoadingPage.jsx
@@ -0,0 +1,32 @@
+import {motion} from "framer-motion";
+import earthAlone from "../../images/earthAlone.svg";
+import React from "react";
+
+export default function LoadingPage() {
+    const pageVariants = {
+        initial: { opacity: 0},
+        animate: { opacity: 1 },
+        exit: { opacity: 0 },
+    };
+
+    const pageTransition = {
+        duration: 4,
+    };
+
+    return (
+        <motion.div
+            className="loader"
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            variants={pageVariants}
+            transition={pageTransition}
+        >
+            <div className="loader-container">
+                <div className="spinner w-8/12">
+                    <img src={earthAlone} alt=""/>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
