ebd8fac0e2e6b061e7711c933182bda3de9db6c6
chado <a.sale@outlook.fr>
2023-11-13 06:55:47 +0100

    Feat: add LoadingPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './component/pages/home';
 import { Contact } from './component/pages/Contact';
 import { Skill } from './component/pages/Skill';
+import LoadingPage from './component/pages/LoadingPage';
 import {AnimatePresence} from "framer-motion";
 
 function App() {
@@ -14,13 +15,12 @@ function App() {
             setLoading(false);
         }, 2000);
     }, []);
+
     return (
         <Router>
             <AnimatePresence>
                 {loading ? (
-                    <div className="loader-container">
-                        <div className="spinner"></div>
-                    </div>
+                    <LoadingPage />
                 ) : (
                 <Routes>
                         <Route path="/" element={<Home />} />
