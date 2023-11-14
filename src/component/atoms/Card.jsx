e1f04b3b1d56bf2fdd542baecafa47914072970c
chado <a.sale@outlook.fr>
2023-11-14 08:39:52 +0100

    Feat: Update flip animation for one card alone WIP && add github repository link in card

--- src/component/atoms/Card.jsx
+++ src/component/atoms/Card.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
-import ReactCardFlip from 'react-card-flip';
-import { Progress } from 'flowbite-react';
+    import React, {useState} from "react";
+    import ReactCardFlip from 'react-card-flip';
+    import { Progress } from 'flowbite-react';
 
-
-export function Card(props) {
+    const flippedCard = [];
+    export function Card(props) {
     let GridElementClass = "text-center py-6 hover:cursor-pointer w-full";
     let GridElementStyle = {
         background: 'rgba(47, 47, 56, 0.7)',
@@ -21,16 +21,60 @@ export function Card(props) {
         fontFamily: "montserrat",
     };
 
+    const linkList = [
+        {
+            name: 'JobItBetter',
+            link: 'https://github.com/Chadowww/JobItBetter',
+            techno: ['HTML', 'CSS', 'PHP','Java Script', 'Symfony', 'Twig' ,'React' , 'Doctrine', 'Twig', 'Bootstrap', 'Mysql', 'Symfony UX', 'Webpack']
+        },
+        {
+            name: 'Project Certification',
+            link: 'https://github.com/Chadowww/Porject_Certification',
+            techno: ['HTML', 'CSS', 'PHP','Java Script', 'Symfony', 'Doctrine', 'Twig', 'TailwindCSS', 'PhpUnit', 'Mysql', 'Symfony UX', 'Webpack']
+        },
+        {
+            name: 'ApiChado',
+            link: 'https://github.com/Chadowww/ApiChado',
+            techno: ['PHP', 'Symfony', 'Doctrine', 'PhpUnit', 'Mysql', 'API Rest']
+        },
+        {
+            name: 'Portfolio-react',
+            link: 'https://github.com/Chadowww/portfolio-react',
+            techno: ['HTML', 'CSS', 'React', 'TailwindCSS', 'Java Script'] },
+        {
+            name: 'EarthWindAndWire',
+            link: 'https://github.com/WildCodeSchool/2023-02-PHP-Bordeaux-P2-EarthWindAndWire',
+            techno: ['HTML', 'CSS', 'PHP' , 'Java Script', 'Bootstrap', 'Mysql'] },
+        {
+            name: 'DHM',
+            link: 'https://github.com/Chadowww/DHM',
+            techno: ['HTML', 'CSS', 'PHP' , 'Laravel', 'Vue.JS', 'TailwindCSS', 'Webpack'] },
+    ];
+
     const [isFlipped, setIsFlipped] = useState(false);
 
     const handleClick = (e) => {
         e.preventDefault();
+
+        let lastCard = flippedCard[flippedCard.length - 1];
+
+        if (flippedCard.length > 0 && lastCard !== props.skill.name) {
+            const previousCard = document.getElementById(lastCard + "-back");
+            previousCard.click()
+            flippedCard.splice(0, flippedCard.length);
+        }
+
         setIsFlipped(!isFlipped);
         let element = document.getElementById(props.skill.name + "-back");
         let grandParent = element.parentElement.parentElement.parentElement.parentElement.parentElement;
         element.classList.toggle("absolute");
         grandParent.classList.toggle("z-[1000]");
+
+        if (isFlipped === false){
+            flippedCard.push(props.skill.name);
+        }
     }
+
     return (
         <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
             <div id={props.skill.name + "-front"} className={GridElementClass} style={GridElementStyle} onClick={handleClick}>
@@ -41,12 +85,17 @@ export function Card(props) {
             </div>
             <div className="relative">
                 <div id={props.skill.name + "-back"} className={GridElementClassBack} style={GridElementStyleBack} onClick={handleClick}>
-                    <ul>
-                        <li><a href="http://"> test de porjet</a></li>
-                        <li><a href="http://"> test de porjet</a></li>
-                        <li><a href="http://"> test de porjet</a></li>
-                        <li><a href="http://"> test de porjet</a></li>
-                        <li><a href="http://"> test de porjet</a></li>
+                    <ul className={"overflow-hidden"}>
+                        {linkList.map((link, index) => {
+                            if (link.techno.includes(props.skill.name)) {
+                                return (
+                                    <li key={index}>
+                                        <a href={link.link} target="_blank" rel="noreferrer" onClick={(e) => e.stopPropagation()}>{link.name}</a>
+                                    </li>
+                                )
+                            }
+                            return null;
+                        })}
                     </ul>
                 </div>
             </div>
