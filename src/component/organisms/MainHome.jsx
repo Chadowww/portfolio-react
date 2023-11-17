9c985df804ba0083e25adf20fbf7bce34dafc7e2
chado <a.sale@outlook.fr>
2023-11-17 13:16:28 +0100

    Feat: add a transition between MainHome and MainProject

--- src/component/organisms/MainHome.jsx
+++ src/component/organisms/MainHome.jsx
@@ -1,25 +1,46 @@
 import * as PropTypes from "prop-types";
 import {ExpandebaleBtn} from "../atoms/ExpandebaleBtn";
 import background from "../../images/bg-home.svg";
+import React, {useRef} from "react";
 
-export function MainHome() {
+export function MainHome(props) {
 
+    const main = useRef();
     let ClassTitle = 'text-6xl font-bold text-center font-[DeathStar] text-shadow my-4';
     let StyleTitle = {
         color: 'transparent',
         WebkitTextStroke: '1px #CEB7FF',
     }
 
-    const ClassHome = 'w-screen h-screen flex flex-col justify-between items-center overflow-hidden';
+    const ClassHome = 'w-screen h-screen flex flex-col justify-between items-center overflow-hidden absolute top-0 left-0 z-10';
     const StyleHome = {
         backgroundImage: `url(${background})`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        backgroundAttachment: "fixed"
+        backgroundAttachment: "fixed",
+        opacity: 1,
     }
+    window.addEventListener('wheel', (e) => {
+        const delta = Math.sign(e.deltaY);
+        const container= main.current;
+        if (container) {
+            let currentOpacity = parseFloat(container.style.opacity);
+
+            if (delta === 1 && currentOpacity >= 0.01) {
+                currentOpacity -= 0.007;
+            }
+
+            if (delta === -1 && currentOpacity <= 1) {
+                currentOpacity += 0.007;
+            }
+
+            container.style.opacity = currentOpacity.toString();
+            console.log(container.style.opacity);
+        }
+    })
     return (
-        <div className={ClassHome} style={StyleHome}>
+        <div className={ClassHome} style={StyleHome} ref={main}>
             <div></div>
             <div className="w-fit animate-pulse">
                 <div className="w-full flex flex-row-reverse justify-between items-center">
