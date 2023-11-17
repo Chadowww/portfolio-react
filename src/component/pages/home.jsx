49623edc7d53f2929014444689801d27f1a867a5
chado <a.sale@outlook.fr>
2023-11-17 13:19:24 +0100

    Feat: Update the home page structure

--- src/component/pages/home.jsx
+++ src/component/pages/home.jsx
@@ -1,21 +1,15 @@
 import * as PropTypes from "prop-types";
 import {MainHome} from "../organisms/MainHome";
+import {MainProject} from "../organisms/MainProject";
 import {RightSideBar} from "../organisms/RightSideBar";
 import {LeftSideBar} from "../organisms/LeftSideBar";
-import background from '../../images/bg-home.svg';
-import {motion} from "framer-motion";
+import {animate, motion, useAnimation} from "framer-motion";
 import {Particle} from "../templates/Particles";
 import React from "react";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 function Home() {
-    const ClassHome = ' relative';
-    const StyleHome = {
-        backgroundImage: `url(${background})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundAttachment: "fixed"
-    }
 
     const pageVariants = {
         initial: { opacity: 0},
@@ -35,14 +29,13 @@ function Home() {
             variants={pageVariants}
             transition={pageTransition}
         >
-            <div className={ClassHome} style={StyleHome}>
-                <Particle />
-                <div className={"h-screen w-full flex relative"} style={{background: 'rgba(47, 47, 56, 0.4)',}}>
-                    <LeftSideBar/>
-                    <MainHome/>
-                    <RightSideBar/>
-                </div>
+            <Particle />
+            <LeftSideBar/>
+            <div className={""} style={{background: 'rgba(47, 47, 56, 0.4)',}}>
+                <MainHome/>
+                <MainProject/>
             </div>
+            <RightSideBar/>
         </motion.div>
     );
 }
