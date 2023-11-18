3dc326199c11ba7d630e150b30fd25a45ac992b6
chado <a.sale@outlook.fr>
2023-11-18 10:06:32 +0100

    Feat: Add responsive for SM MD and lG screen

--- src/component/organisms/RightSideBar.jsx
+++ src/component/organisms/RightSideBar.jsx
@@ -8,16 +8,15 @@ export function RightSideBar() {
     useEffect(() => {
         if (window.location.pathname === "/skill") {
             setIsShow(false);
-            console.log("isShown false");
         }
     }, []);
 
-    let contactClass = "text-4xl font-bold text-center font-[DeathStar] text-shadow animate-pulse";
+    let contactClass = "text-1xl md:text-2xl lg:text:4xl font-bold text-center font-[DeathStar] text-shadow animate-pulse";
     let contactStyle = {
         color: "transparent",
         WebkitTextStroke: "1px #CEB7FF",
     };
-    let scollClass = "font-bold text-center font-[DeathStar] text-shadow m-12 animate-bounce";
+    let scollClass = "hidden md:block font-bold text-center font-[DeathStar] text-shadow m-12 animate-bounce";
     let scrollStyle = {
         color: "transparent",
         WebkitTextStroke: "1px #CEB7FF",
@@ -25,7 +24,7 @@ export function RightSideBar() {
     };
     return (
         <div className="fixed  top-0 right-0  z-10 h-full bg-yellow flex flex-col justify-between items-end">
-            <div className={"m-4"}>
+            <div className={" w-full m-1 text-right md:text-center md:transform-none"}>
                 <a href="/Contact" className={contactClass} style={contactStyle}>contact me</a>
             </div>
             <div className={scollClass} style={scrollStyle}>
