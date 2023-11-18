40def9a4578984a1e3e295ba9804a50f905b77f6
chado <a.sale@outlook.fr>
2023-11-18 10:02:40 +0100

    Feat: Add props to add message in component add key and updte width

--- src/component/atoms/ExpandebaleBtn.jsx
+++ src/component/atoms/ExpandebaleBtn.jsx
@@ -3,7 +3,7 @@ import * as PropTypes from "prop-types";
 import vector1 from "../../images/vector/Vector1.svg";
 import vector2 from "../../images/vector/Vector2.svg";
 
-export function ExpandebaleBtn() {
+export function ExpandebaleBtn(props) {
 
     const [isOpen, setIsOpen] = useState(false);
     const handleClick = () => {
@@ -24,7 +24,8 @@ export function ExpandebaleBtn() {
 
     const StyleSpitch = {
         height: isOpen ? "200px" : "0",
-        width: isOpen ? "750px" : "0",
+        width: isOpen ? "700px" : "0",
+        position: isOpen ? "relative" : "absolute",
         fontFamily: "DeathStar",
         fontSize: "25px",
         color: "#CEB7FF",
@@ -32,6 +33,7 @@ export function ExpandebaleBtn() {
         background: 'rgba(47, 47, 56, 0.7)',
         transition: isOpen ? "height 1s, width 1s, display 1s" : "height 1s , width 1s, display 1s",
         transform: isOpen ? "scale(1)" : "scale(0)",
+        overflow: "scroll",
     };
     let containerExpandebale = {
         transition: isOpen ? "width 2s, height 2s" : "width 2s , height 2s",
@@ -42,16 +44,17 @@ export function ExpandebaleBtn() {
             className={"w-fit h-fit p-8 relative cursor-pointer justify-center items-center"}
             style={containerExpandebale}
             onClick={handleClick}
+            key={props.id}
         >
-            <div className={"absolute top-[0] left-[0] col-start-1 col-end-12 vector-pulse"}>
+            <div className={"absolute top-0 left-0 col-start-1 col-end-12 vector-pulse"}>
                 <img src={vector1} alt="vector1" className={""} />
             </div>
-            <div className={"flex justify-center items-center "} >
+            <div className={"flex justify-center items-center relative "} >
                 <h2 className={ClassTitle} style={StyleTitle}>
                     CLICK <br /> TO OPEN
                 </h2>
-                <span className={"overflow-hidden text-center p-6 spitch"} style={StyleSpitch}>
-                    "My goal is to turn your ideas into digital reality. I focus on code quality, security, and efficiency, and I am capable of managing all aspects of your code design."
+                <span className={" top-0 left-0 overflow-hidden text-center p-6 spitch"} style={StyleSpitch}>
+                    {props.message}
                 </span>
             </div>
             <div className={"absolute bottom-[0] right-[0] col-start-1 col-end-12 justify-end vector-pulse"}>
