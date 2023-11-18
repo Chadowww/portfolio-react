3218e8eec511cdfedd3db2b5e399a0e6da078ad0
chado <a.sale@outlook.fr>
2023-11-18 10:06:02 +0100

    Feat: Add carousel for show each project and and responsive fot SM MD and LG screen

--- src/component/organisms/MainProject.jsx
+++ src/component/organisms/MainProject.jsx
@@ -1,9 +1,56 @@
 import * as PropTypes from "prop-types";
 import {ExpandebaleBtn} from "../atoms/ExpandebaleBtn";
+import {Carousel} from "flowbite-react";
 import bgproject from '../../images/bg-project.svg';
 
 export function MainProject() {
-    let ClassProject = 'w-screen h-screen flex flex-col justify-between items-center absolute top-0 left-0 z-0';
+
+    const projects = [
+        {
+            id: 1,
+            name: 'JobItBetter',
+            link: 'https://github.com/Chadowww/JobItBetter',
+            techno: ['HTML', 'CSS', 'PHP','Java Script', 'Symfony', 'Twig' ,'React' , 'Doctrine', 'Twig', 'Bootstrap', 'Mysql', 'Symfony UX', 'Webpack'],
+            message: 'Projet de fin de formation, il a ete repris par la suite pour etre ameliore. Il s\'agit d\'une plateforme de mise en relation entre entreprises et demandeurs d\'emploi. Le projet est en cours de développement. Projet de fin de formation, il a ete repris par la suite pour etre ameliore. Il s\'agit d\'une plateforme de mise en relation entre entreprises et demandeurs d\'emploi. Le projet est en cours de développement.',
+        },
+        {
+            id: 2,
+            name: 'Project Certification',
+            link: 'https://github.com/Chadowww/Porject_Certification',
+            techno: ['HTML', 'CSS', 'PHP','Java Script', 'Symfony', 'Doctrine', 'Twig', 'TailwindCSS', 'PhpUnit', 'Mysql', 'Symfony UX', 'Webpack'],
+            message: 'Projet developpe dans le but de passer la certification RNCP. Il s\'agit d\'une plateforme de libraire en ligne. Le projet n\'est pas arrive à sont therme.',
+        },
+        {
+            id: 3,
+            name: 'ApiChado',
+            link: 'https://github.com/Chadowww/ApiChado',
+            techno: ['PHP', 'Symfony', 'Doctrine', 'PhpUnit', 'Mysql', 'API Rest'],
+            message: 'Refonte en cours du projet JobItBetter le but etant de travailler comme des projets réel fonctionnant avec un projet front et un projet back et une API pour la communication.',
+        },
+        {
+            id: 4,
+            name: 'Portfolio-react',
+            link: 'https://github.com/Chadowww/portfolio-react',
+            techno: ['HTML', 'CSS', 'React', 'TailwindCSS', 'Java Script'],
+            message: 'Projet de portfolio realise en React actuellement en developpement.',
+        },
+        {
+            id: 5,
+            name: 'EarthWindAndWire',
+            link: 'https://github.com/WildCodeSchool/2023-02-PHP-Bordeaux-P2-EarthWindAndWire',
+            techno: ['HTML', 'CSS', 'PHP' , 'Java Script', 'Bootstrap', 'Mysql'],
+            message: 'Premier projet en POO sans framework php, il s\'agit d\'un reseau social.',
+        },
+        {
+            id: 6,
+            name: 'DHM',
+            link: 'https://github.com/Chadowww/DHM',
+            techno: ['HTML', 'CSS', 'PHP' , 'Laravel', 'Vue.JS', 'TailwindCSS', 'Webpack'],
+            message: 'Projet de site vitrine pour un client, il s\'agit d\'un site de maintenant et de vente informatique.',
+        },
+    ];
+
+    let ClassProject = 'w-screen h-screen flex flex-col justify-between items-center absolute top-0 left-0 z-0 earth-container-project overflow-hidden';
     let StyleProject = {
         backgroundColor: '#2D0056',
         backgroundImage: `url(${bgproject})`,
@@ -11,6 +58,7 @@ export function MainProject() {
         backgroundSize: "cover",
         backgroundPosition: "center",
     };
+
     let ClassTitle = 'text-6xl font-bold text-center font-[DeathStar] text-shadow my-4';
     let StyleTitle = {
         color: 'transparent',
@@ -33,11 +81,22 @@ export function MainProject() {
                     <div className="w-full h-4  border-glow"></div>
                 </div>
             </div>
-            <div className={'relative earth-container-project flex justify-center items-end  w-full min-h-[400px] overflow-hidden'}>
-                <div className={'p-6 absolute z-10'}>
-                    <ExpandebaleBtn/>
-                </div>
-            </div>
+            <Carousel indicators={false} slide={false} className={"w-6/12 h-2/5 overflow-hidden"}>
+                {projects.map((project, index) =>{
+                    return(
+                        <div className={"h-full w-full"} key={project.id}>
+                            <div className={"w-full flex flex-col justify-end"}>
+                                <a href={project.link}><h2 className={"text-center text-4xl font-bold"}> Project: {project.name}</h2></a>
+                                <div className={'relative  flex justify-center items-end  w-full min-h-[400px] overflow-hidden'}>
+                                    <div className={'p-6 absolute z-50'}>
+                                        <ExpandebaleBtn message={project.message}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </Carousel>
         </div>
     )
 }
