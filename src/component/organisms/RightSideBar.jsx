3fc1df61270c654253f8c0c2a6d4ca3554ed29c1
chado <a.sale@outlook.fr>
2023-11-19 14:24:53 +0100

    Feat: Update responsive

--- src/component/organisms/RightSideBar.jsx
+++ src/component/organisms/RightSideBar.jsx
@@ -11,23 +11,23 @@ export function RightSideBar() {
         }
     }, []);
 
-    let contactClass = "text-1xl md:text-2xl lg:text:4xl font-bold text-center font-[DeathStar] text-shadow animate-pulse";
+    let contactClass = "text-1xl md:text-2xl lg:text-4xl mt-8 font-bold text-center font-[DeathStar] text-shadow animate-pulse";
     let contactStyle = {
         color: "transparent",
         WebkitTextStroke: "1px #CEB7FF",
     };
-    let scollClass = "hidden md:block font-bold text-center font-[DeathStar] text-shadow m-12 animate-bounce";
+    let scrollClass = "hidden md:block font-bold text-center font-[DeathStar] text-shadow animate-bounce";
     let scrollStyle = {
         color: "transparent",
         WebkitTextStroke: "1px #CEB7FF",
         opacity: isShow ? 1 : 0,
     };
     return (
-        <div className="fixed  top-0 right-0  z-10 h-full bg-yellow flex flex-col justify-between items-end">
+        <div className="fixed top-0 right-0 z-10 md:h-full p-8 flex flex-col justify-between items-end">
             <div className={" w-full m-1 text-right md:text-center md:transform-none"}>
                 <a href="/Contact" className={contactClass} style={contactStyle}>contact me</a>
             </div>
-            <div className={scollClass} style={scrollStyle}>
+            <div className={scrollClass} style={scrollStyle}>
                 <h3 className={"uppercase"}>scroll</h3>
                 <img src={scrollSvg} alt="scroll"/>
             </div>
@@ -37,6 +37,6 @@ export function RightSideBar() {
 RightSideBar.prototype = {
     contactClass: PropTypes.string,
     contactStyle: PropTypes.object,
-    scollClass: PropTypes.string,
+    scrollClass: PropTypes.string,
     scrollStyle: PropTypes.object,
 };
