eef29016f98cfe10834d9ea692242505804001e6
chado <a.sale@outlook.fr>
2023-11-20 13:01:24 +0100

    Feat: Add z-index fore be under Home Page

--- src/component/templates/Particles.jsx
+++ src/component/templates/Particles.jsx
@@ -7,7 +7,7 @@ export function Particle() {
         position: "fixed",
         width: "100%",
         height: "100vh",
-        zIndex: "11",
+        zIndex: "-1",
     };
 
     useEffect(() => {
