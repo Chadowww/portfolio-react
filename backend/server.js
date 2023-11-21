c79f013203de1aa549236c0db1bc89ce40e62fd3
chado <a.sale@outlook.fr>
2023-11-21 16:40:11 +0100

    Feat: Update route file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const port = 3001;
-const routes = require('./routes');
+const routes = require('./routes/routes');
 const cors = require('cors');
 const app = express();
 app.use(cors());
