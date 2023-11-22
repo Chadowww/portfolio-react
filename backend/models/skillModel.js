f0bd08690d0a1eb8936d71df0553f52a9787265c
chado <a.sale@outlook.fr>
2023-11-22 10:44:57 +0100

    feat: Create functions to get skills and project by skills

--- backend/models/skillModel.js
+++ backend/models/skillModel.js
@@ -19,6 +19,25 @@ const SkillModel = {
             connection.end();
         }
     },
+
+    getAllSkillsWithProjects: async () => {
+        const connection = await mysql.createConnection({
+            host: "127.0.0.1",
+            user: "CHADO",
+            password: "wow",
+            database: "portfolio"
+        });
+
+        try {
+            const [rows] = await connection.execute("SELECT project_id, skill_id, projects.name, projects.link FROM projects_skills INNER JOIN skills ON projects_skills.skill_id = skills.id INNER JOIN projects ON projects_skills.project_id = projects.id order by skill_id, project_id");
+            return rows;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        } finally {
+            connection.end();
+        }
+    },
 };
 
 module.exports = SkillModel;
