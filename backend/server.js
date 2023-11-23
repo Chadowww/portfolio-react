4b1c77228c8b2b888f247cd9057b13fcb0f64ac6
chado <a.sale@outlook.fr>
2023-11-23 12:20:56 +0100

    Feat: refactor code

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,11 @@ const port = 3001;
 const routes = require('./routes/routes');
 const cors = require('cors');
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 5000');
 });
