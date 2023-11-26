f8dc8285299f5c6313c1aab0150a2902c77d055b
chado <a.sale@outlook.fr>
2023-11-26 10:18:50 +0100

    Feat: Created new page Project

--- src/component/pages/Project.jsx
+++ src/component/pages/Project.jsx
@@ -1,7 +1,31 @@
+import {motion} from 'framer-motion';
+import {RightSideBar} from "../organisms/RightSideBar";
+import {LeftSideBar} from "../organisms/LeftSideBar";
+import {MainProjectContent} from "../organisms/MainProjectContent";
+
 export function Project() {
+
+    const pageVariants = {
+        initial: { opacity: 0},
+        animate: { opacity: 1 },
+        exit: { opacity: 0 },
+    };
+
+    const pageTransition = {
+        duration: 3,
+    };
+
     return (
-        <div>
-            <h1>Project</h1>
-        </div>
+        <motion.div
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageVariants}
+            transition={pageTransition}
+        >
+            <LeftSideBar/>
+            <MainProjectContent/>
+            <RightSideBar/>
+        </motion.div>
     )
 }
